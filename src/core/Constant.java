cad18fffb72d9f5936aeca4d486a8734b097c8cd
martinbelica <mato.belica@gmail.com>
2016-10-12 21:58:22 +0200

    initial commit

---
+++ src/core/Constant.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author martin
+ */
+public class Constant extends Literal { 
+
+    private String value;
+
+    public Constant(String value) {
+        this.value = value;
+    }
+
+    /**
+     * @return the value
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public int compareTo(Literal t) {
+        return (this.value.equals(((Constant) t).value) ? 0 : -1);
+    }
+
+    public String toString() {
+        return this.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.value.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Constant other = (Constant) obj;
+        if (!Objects.equals(this.value, other.value)) {
+            return false;
+        }
+        return true;
+    }
+}
